d1d6e9b92ebfd628ff47f3981cdfe456a5d411eb
Webeau24 <Webeau24@outlook.com>
2025-04-04 19:44:26 +0800

    这是第一次提交

---
+++ test.py
@@ -0,0 +1,4 @@
+import pandas as pd 
+confirmed_data = pd.read_csv('time_series_covid19_confirmed_global.csv')
+deaths_data = pd.read_csv('time_series_covid19_deaths_global.csv')
+recovered_data = pd.read_csv('time_series_covid19_recovered_global.csv')
